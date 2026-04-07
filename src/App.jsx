19cc65367d6ed1afd0c1686ef1dba8c7be54eace
Kie Croft <kiecroft@gmail.com>
2026-04-07 21:33:04 +0100

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./css/App.css"
+import { BootScreen } from './screens/BootScreen'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeScreen, setActiveScreen] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='parent-container'>
+      <div className='inner-container'>
+        <div className="title-outer">
+          <div className="title-inner">
+            <h1>== AI ADVENTURE GAME ==</h1>
+          </div>
+        </div>
+        <div className="active-section-outer">
+          <div className="active-section-inner-upper">
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <div className="active-section-inner-lower">
+            {
+              activeScreen === 0 ? <BootScreen/> : <></>
+            }
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/screens/BootScreen.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import "../css/BootScreen.css";
+
+
+export const BootScreen = () => {
+  const [activePhrase, setActivePhrase] = useState()
+  const [loops, setLoops] = useState(0)
+  const phrases = [
+    "Loading the mainframe...", 
+    "Warming up the world engine...", 
+    "Brushing off the editor...",
+    "Tweaking the nuts and bolts...",
+    "Coffee break, back in 5 minutes...",    
+  ]
+
+  useEffect(() => {
+    const pickRandom = () => {
+      const randomIndex = Math.floor(Math.random() * phrases.length);
+      setActivePhrase(phrases[randomIndex]);
+      setLoops(loops => loops + 1);
+    };
+    pickRandom();
+    const intervalId = setInterval(pickRandom, 1500);
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  return (
+      <div className="screen-container">
+        {loops >= 10
+        ?  <button>Boot the Adventure</button>
+        : <h2 className="loading-text">{activePhrase}</h2>}
+      </div>
+  );
+}
