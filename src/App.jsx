cef774f1bc1aa60c39c7dd3538e8576a5ac6cf54
Kie Croft <kiecroft@gmail.com>
2026-04-08 07:53:37 +0100

    Added Sass & Updated Stylesheets
    Installed Sass, updated current stylesheets with variables and nesting.
    
    Also created blank MainMenu functional component for next update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import "./css/App.css"
-import { BootScreen } from './screens/BootScreen'
+import { useState } from "react";
+import "./css/App.css";
+import { BootScreen } from "./screens/BootScreen";
 
 function App() {
-  const [activeScreen, setActiveScreen] = useState(0)
+  const [activeScreen, setActiveScreen] = useState(0);
 
   return (
-    <div className='parent-container'>
-      <div className='inner-container'>
+    <div className="parent-container">
+      <div className="inner-container">
         <div className="title-outer">
           <div className="title-inner">
             <h1>== AI ADVENTURE GAME ==</h1>
@@ -19,14 +19,18 @@ function App() {
             <div className="line"></div>
           </div>
           <div className="active-section-inner-lower">
-            {
-              activeScreen === 0 ? <BootScreen/> : <></>
-            }
+            {activeScreen === 0 ? (
+              <BootScreen />
+            ) : activeScreen === 1 ? (
+              <MainMenu />
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
